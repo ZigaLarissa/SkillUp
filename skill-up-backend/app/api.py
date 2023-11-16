33d045d384261039d8936f561b9a44b7e9f47c8f
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-11-16 14:53:18 +0200

    updated the api file with get and post routers

--- skill-up-backend/app/api.py
+++ skill-up-backend/app/api.py
@@ -1,15 +1,29 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine) #creating all tables in the database
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#cors origins
 origins = [
     "http://localhost:3000",
     "localhost:3000"
 ]
 
-
+#adding cors middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -18,7 +32,37 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+#creating routes for the homepage
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
-@app.get("/", tags=["root"])
-async def read_root() -> dict:
-    return {"message": "Welcome to SkillUp."}
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="user not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(dg, skip=skip, limit=limit)
+    return items
