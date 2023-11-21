c8f4ed3263e823c71025a701fd34a395f5aa09ae
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-11-21 14:49:02 +0200

    created components for New habit screen

---
+++ skill-up-frontend/app/screens/NewHabit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function NewHabit(props) {
+    return (
+        <View>
+            <Text>New Habit</Text>
+        </View>
+    );
+}
+
+export default NewHabit;
