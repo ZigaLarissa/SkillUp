72a994596b39127a3087b1242b8911c55bcb278b
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-12-01 18:53:59 +0200

    updated models.py for sqlalchemy databases

---
+++ skill-up-backend/app/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from blogs.database import Base
+from sqlalchemy.orm import relationship
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    creator = relationship("User", back_populates="tasks")
+
+
+class User(Base):
+    __tablename__   = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+
+    tasks = relationship("Blog", back_populates="creator")
